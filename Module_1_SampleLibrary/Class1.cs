1d143e6c8095a276816bc1a81220b9e7ed94c1bb
nilesh-shirude <103946687+nilesh-shirude@users.noreply.github.com>
2023-04-30 19:31:17 +0530

    Module-1 Dot net core actual Library sample

---
+++ Module_1_SampleLibrary/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module_1_SampleLibrary
+{
+    public class HelloWorld
+    {
+        public static string GetHelloWorldString(string username)
+        {
+            string currentTime = DateTime.Now.ToString("HH:mm:ss");
+            return $"{currentTime} Hello, {username}!";
+        }
+    }
+}
