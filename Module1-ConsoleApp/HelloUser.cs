7b520c6487be9a463083843dc35b8882acabf7cc
Nilsboom23 <131838593+Nilsboom23@users.noreply.github.com>
2023-04-30 18:36:02 +0530

    Module-1 console app code

---
+++ Module1-ConsoleApp/HelloUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Module_1_SampleLibrary;
+
+namespace Module1_ConsoleApp
+{
+    internal class HelloUser
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string username = args[0];
+
+                //Without using sample_Library
+                // Console.WriteLine($"Hello, {username}!");
+
+                //using sample_Library
+                string helloWorldString = HelloWorld.GetHelloWorldString(username);
+                Console.WriteLine(helloWorldString);
+            }
+            else
+            {
+                Console.WriteLine("Please provide a username as a command line argument.");
+            }
+        }
+    }
+}
+
+
