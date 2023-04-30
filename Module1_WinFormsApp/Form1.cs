69f41159d7d9b0acdc2c7d5be6cc32fa48ed78d5
nilesh-shirude <103946687+nilesh-shirude@users.noreply.github.com>
2023-04-30 19:27:46 +0530

    Module1- Dot Net Sample Library

---
+++ Module1_WinFormsApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Module_1_SampleLibrary;
+
+namespace Module1_WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // Method intentionally left empty.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //without using Library code below
+            //string message = "Hello " + textBox1.Text;
+
+            //Using Library
+            string message = HelloWorld.GetHelloWorldString(textBox1.Text);
+            string title = "Demo";
+            MessageBox.Show(message, title);
+        }
+    }
+}
