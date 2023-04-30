61fac34bfd2b9ecc80464cee8bdff7d82eb896a6
nilesh-shirude <103946687+nilesh-shirude@users.noreply.github.com>
2023-04-30 19:21:13 +0530

    Dot net core console app

---
+++ Module1-ConsoleApp/HelloUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module1_ConsoleApp
+{
+    internal class HelloUser
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string username = args[0];
+                Console.WriteLine($"Hello, {username}!");
+            }
+            else
+            {
+                Console.WriteLine("Please provide a username as a command line argument.");
+            }
+        }
+    }
+}
